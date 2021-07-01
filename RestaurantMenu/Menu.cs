e166b47b381cb2c6a7c973e01403beb502093788
Charlie <Charlie@Carde-MacBook.local>
2021-06-30 20:00:37 -0400

    refactored the code into classes. Complete

--- RestaurantMenu/Menu.cs
+++ RestaurantMenu/Menu.cs
@@ -6,6 +6,10 @@ namespace RestaurantMenu
     public class Menu
     {
         public string FoodCatagories { get; set; }
+        public static Menu appetizers = new Menu("Appetizers");
+        public static Menu mainCourse = new Menu("Main Course");
+        public static Menu dessert = new Menu("Dessert");
+        public static Menu newItems = new Menu("**Brand New Selection**");
         public Menu(string afood)
         {
             FoodCatagories = afood;
@@ -14,9 +18,19 @@ namespace RestaurantMenu
         {
             return value.ToString("yyyy/MM/dd/HHmm");
         }
-       
+        public static void PrintMenu()
+        {
+            Console.WriteLine("\tTacos 'n More");
+            Console.WriteLine(appetizers.FoodCatagories);
+            Console.WriteLine($"{MenuItem.jalapeno.Food}\n{MenuItem.jalapeno.FoodDescription}\nPrice: ${MenuItem.jalapeno.FoodPrice}\n");
+            Console.WriteLine($"{MenuItem.stuffedPeppers.Food}\n{MenuItem.stuffedPeppers.FoodDescription}\nPrice: ${MenuItem.stuffedPeppers.FoodPrice}\n");
+            Console.WriteLine(mainCourse.FoodCatagories);
+            Console.WriteLine($"{MenuItem.tacosCarneAsada.Food}\n{MenuItem.tacosCarneAsada.FoodDescription}\nPrice: ${MenuItem.tacosCarneAsada.FoodPrice}\n");
+            Console.WriteLine($"{MenuItem.torta.Food}\n{MenuItem.torta.FoodDescription}\nPrice: ${MenuItem.torta.FoodPrice}\n");
+            Console.WriteLine(dessert.FoodCatagories);
+            Console.WriteLine($"{MenuItem.friedIceCream.Food}\n{MenuItem.friedIceCream.FoodDescription}\nPrice: ${MenuItem.friedIceCream.FoodPrice}\n");
+        }
 
-        //new food item
-        
+
     }
 }
